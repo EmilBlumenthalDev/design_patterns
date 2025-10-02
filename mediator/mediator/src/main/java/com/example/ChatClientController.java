9b7ace1a910ed26bfe99b9062659d2d8cf31c6e7
litioner <litioner@protonmail.com>
2025-10-02 11:13:41 +0300

    Remove comment about clearing chat area on recipient change in ChatClientController.

--- mediator/mediator/src/main/java/com/example/ChatClientController.java
+++ mediator/mediator/src/main/java/com/example/ChatClientController.java
@@ -24,7 +24,6 @@ public class ChatClientController implements ChatClient {
         mediator.registerClient(this);
         refreshRecipients();
         
-        // Clear chat area when recipient changes
         recipientBox.setOnAction(e -> chatArea.clear());
     }
 
